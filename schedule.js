ecb518b7bff70aa378c4f59422c60ca92c6a4098
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2023-11-17 19:31:55 -0600

    works on one schedule

--- schedule.js
+++ schedule.js
@@ -1,45 +1,52 @@
 const ics = require("ics");
 const fs = require("fs");
-const dates = [
-  {
-    startDate: "2023-11-25T13:30:00+00:00",
-    endDate: "2023-11-26T01:47:00+00:00",
-  },
-];
 
-const startDate = new Date(dates[0].startDate);
-const endDate = new Date(dates[0].endDate);
+const schedules = [{
+  "start_timestamp": "2023-11-14T13:30:00+00:00",
+  "display_str": "RN 2023-11-14 07:30:00-19:47:00 956R215",
+  "shift_class": "Day",
+  "shift_icon": "DX",
+  "payroll_code": 66,
+  "department": 4166,
+  "employee": 6195234,
+  "date": "2023-11-14",
+  "duration_hours": 11.78,
+  "end_timestamp": "2023-11-15T01:47:00+00:00",
+  "id": 1428702234,
+  "occupation": 398
+}]
 
-ics.createEvent(
-  {
-    start: [
-      startDate.getUTCFullYear(),
-      startDate.getUTCMonth() + 1,
-      startDate.getUTCDate(),
-      startDate.getUTCHours(),
-      startDate.getUTCMinutes(),
-    ],
-    title: "Shift",
-    end: [
-      endDate.getUTCFullYear(),
-      endDate.getUTCMonth() + 1,
-      endDate.getUTCDate(),
-      endDate.getUTCHours(),
-      endDate.getUTCMinutes(),
-    ],
-  },
-  (error, value) => {
-    if (error) {
-      console.log(error);
+const events = schedules.map(schedule => {
+  const startDate = new Date(schedule.start_timestamp);
+  const endDate = new Date(schedule.end_timestamp);
+  const shiftTitle = schedule.shift_class;
+    return {
+      startInputType: "utc",
+      start: [
+        startDate.getUTCFullYear(),
+        startDate.getUTCMonth() + 1,
+        startDate.getUTCDate(),
+        startDate.getUTCHours(),
+        startDate.getUTCMinutes(),
+      ],
+      title: shiftTitle,
+      endInputType: "utc",
+      end: [
+        endDate.getUTCFullYear(),
+        endDate.getUTCMonth() + 1,
+        endDate.getUTCDate(),
+        endDate.getUTCHours(),
+        endDate.getUTCMinutes(),
+      ],
     }
+})
 
-    fs.writeFile("event.ics", value, (err) => {
-      if (err) {
-        console.log("Error writing file:", err);
-      } else {
-        console.log("File written successfully");
-      }
-    });
-    console.log(value);
-  }
-);
+const {error, value} = ics.createEvents(events)
+
+fs.writeFile("event.ics", value, (err) => {
+        if (err) {
+          console.log("Error writing file:", err);
+        } else {
+          console.log("File written successfully");
+        }
+      });
